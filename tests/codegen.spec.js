e789ed24faf485d486d202bcbff3966131c269c4
rezwanulislamrimel <rezwanul.rimel97@gmail.com>
2026-09-25 03:33:01 +0600

    added Codegen automation

---
+++ tests/codegen.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/');
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.getByRole('dialog', { name: 'Log in' }).click();
+  await page.locator('#loginusername').click();
+  await page.locator('#loginusername').fill('Rimel');
+  await page.locator('#loginusername').press('Tab');
+  await page.locator('#loginpassword').fill('Test@1234');
+  await page.locator('#loginpassword').press('Enter');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByText('PRODUCT STORE Home (current)').click();
+  await page.getByRole('link', { name: 'Welcome Rimel' }).click();
+});
